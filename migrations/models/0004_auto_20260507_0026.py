d18f90f8b3eefbbdef9ab6f995a11d28accdbe06
Kirill Ibragimov <hexchap@gmail.com>
2026-05-07 00:34:46 +0300

    fix: correct SQLOperation signature

--- migrations/models/0004_auto_20260507_0026.py
+++ migrations/models/0004_auto_20260507_0026.py
@@ -7,8 +7,5 @@ class Migration(Migration):
     initial = False
 
     operations = [
-        SQLOperation(
-            sql='ALTER TABLE "payments" ADD COLUMN IF NOT EXISTS "locale" VARCHAR(10) NOT NULL DEFAULT \'en\';',
-            reverse_sql='ALTER TABLE "payments" DROP COLUMN IF EXISTS "locale";',
-        ),
+        SQLOperation('ALTER TABLE "payments" ADD COLUMN IF NOT EXISTS "locale" VARCHAR(10) NOT NULL DEFAULT \'en\';'),
     ]
