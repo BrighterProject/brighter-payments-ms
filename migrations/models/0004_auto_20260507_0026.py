ee8f6c4cee36c8dd95a4b0665a379257545ec7ec
Kirill Ibragimov <hexchap@gmail.com>
2026-05-07 00:32:12 +0300

    fix: use SQLOperation for locale column migration

--- migrations/models/0004_auto_20260507_0026.py
+++ migrations/models/0004_auto_20260507_0026.py
@@ -1,17 +1,14 @@
-from tortoise import migrations
+from tortoise.migrations import Migration, SQLOperation
 
 
-class Migration(migrations.Migration):
+class Migration(Migration):
     dependencies = [('models', '0003_auto_20260506_2345')]
 
     initial = False
 
     operations = [
-        migrations.RunSQL(
-            sql="""
-                ALTER TABLE "payments"
-                ADD COLUMN IF NOT EXISTS "locale" VARCHAR(10) NOT NULL DEFAULT 'en';
-            """,
+        SQLOperation(
+            sql='ALTER TABLE "payments" ADD COLUMN IF NOT EXISTS "locale" VARCHAR(10) NOT NULL DEFAULT \'en\';',
             reverse_sql='ALTER TABLE "payments" DROP COLUMN IF EXISTS "locale";',
         ),
     ]
